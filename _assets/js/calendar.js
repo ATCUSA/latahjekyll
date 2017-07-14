337f9bf26bee3921725d21f197687f5d0f71a8b3
Austin Cole <mail@acole.us>
2017-07-14 10:30:59 -0700

    Added calendar option.

---
+++ _assets/js/calendar.js
@@ -0,0 +1,69 @@
+function calendarList(eventPath, futrAmt, futrIncr) {
+
+	$(document).ready(function() {
+	  $('#calendar').fullCalendar({
+	    header: {
+					left: 'false',
+					center: 'false',
+					right: 'false'
+				},
+				defaultView: 'list',
+				visibleRange: function(currentDate) {
+	        return {
+	          start: currentDate.clone(),
+	          end: currentDate.clone().add(futrAmt, futrIncr)
+        	};
+    		},
+	      eventClick: function(event) {
+					alert(event.title + " - " + event.information)
+	        return false;
+	      events: eventPath,
+	      loading: function(bool) {
+	  			$('#loading').toggle(bool);
+					$('.fc-toolbar').remove();
+	  		},
+				eventRender: function(event, element) {
+					element.find('.fc-list-item-title').append(" - " + event.information);
+				},
+	  });
+	});
+}
+
+function calendarMonth(eventPath) {
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    header: {
+				left: 'prev today next',
+				center: 'title',
+				right: 'month year listYear'
+			},
+			eventOrder:'title,start,end',
+      eventClick: function(event) {
+				alert(event.title + " - " + event.information)
+        return false;
+  		},
+      events: eventPath,
+      loading: function(bool) {
+  			$('#loading').toggle(bool);
+  		},
+			eventRender: function(event, element) {
+				element.find('.fc-list-item-title').append(" - " + event.information);
+			},
+  });
+});
+}
+
+// Snippets
+
+/*
+views: {
+	listThreeMonths: {
+		type: 'list',
+		duration: {
+			month: 3
+		},
+		listDayAltFormat: 'dddd'
+	},
+	eventOrder: 'title,start,end',
+},
+*/
