c677d070f8208303c641fd18509680d2e2a2346f
Austin Cole <mail@acole.us>
2017-07-20 08:21:37 -0700

    Added demo calendar.

--- _assets/js/calendar.js
+++ _assets/js/calendar.js
@@ -53,6 +53,46 @@ $(document).ready(function() {
 });
 }
 
+function listThreeMonths() {
+	$(document).ready(function() {
+
+	  $('#calendar').fullCalendar({
+	    header: {
+					left: 'false',
+					center: 'false',
+					right: 'false'
+				},
+				defaultView: 'listThreeMonths',
+				views: {
+					listThreeMonths: {
+						type: 'list',
+						duration: {
+							month: 3
+						},
+						listDayAltFormat: 'dddd'
+					},
+					eventOrder: 'title,start,end',
+				},
+	      eventClick: function(event) {
+					alert(event.title + " - " + event.information)
+	        // window.open(event.url);
+	        return false;
+	  		// 	// opens events in a popup window
+	  		// 	window.open(event.url, 'gcalevent', 'width=700,height=600');
+	  		// 	return false;
+	  		},
+	      events: '/api/electionCalendar.json',
+	      loading: function(bool) {
+	  			$('#loading').toggle(bool);
+					$('.fc-toolbar').remove();
+	  		},
+				eventRender: function(event, element) {
+					element.find('.fc-list-item-title').append(" - " + event.information);
+				},
+	  });
+	});
+}
+
 // Snippets
 
 /*
