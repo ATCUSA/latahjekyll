0eb85d70bddc48b631e436d2ce9208ad0a7b4a4d
tcherry <tscherry70@gmail.com>
2017-10-20 10:06:38 -0700

    Added news processing for assessors, fixed some other issues

--- _assets/js/news.js
+++ _assets/js/news.js
@@ -1,39 +1,40 @@
-function getNewsandNotices(id, dept, type){
+//args is an array of elements
+function getNewsandNotices(args, dept){
 
   $.ajax({
     url: "https://www.latah.id.us/api/getnewsnotice?dept=" + dept,
     type: "GET",
     async: true,
     dataType: 'json',
-    success: function(data){
-      $('#' + id).empty();
-      $.each(data, function(){
+    success: function(data){      
+      $.each(data, function(index){
+        for(var i=0; i < args.length; i++){
+          if(args[i].indexOf(data[index].ArticleType) !== -1){
+            var link=null;
+            if(data[index].PdfPath != null){
+              $('#' + args[i]).append(
+                $('<a class="list-group-item text-center list-link" ' +
+                ' href="#">' +  data[index].ArticleTitle + '</a>')
+              )
+              $('#' + args[i]).click({param1: data[index].PdfPath}, downloadFile);
 
-        if(type == this.ArticleType){
-          var link=null;
-          if(this.PdfPath != null){
-            $('#' + id).append(
-              $('<a class="list-group-item text-center list-link" ' +
-              ' href="#">' +  this.ArticleTitle + '</a>')
-            )
-            $('#' + id).click({param1: this.PdfPath}, downloadFile);
-
-            function downloadFile(event){
-              if(event.data.param1 != null){
-                var url = 'https://www.latah.id.us/api/downloadFile?fileName=' + event.data.param1;
-                setTimeout(() => window.open(url, '_blank'), 500);
+              function downloadFile(event){
+                if(event.data.param1 != null){
+                  var url = 'https://www.latah.id.us/api/downloadFile?fileName=' + event.data.param1;
+                  setTimeout(() => window.open(url, '_blank'), 500);
+                }
               }
             }
-          }
-          else if(this.Url != null){
-            $('#' + id).append(
-              $('<a class="list-group-item text-center list-link" ' +
-              'href="' + this.Url + '">' +
-              this.ArticleTitle +
-              '</a>')
-            ) //append
-          }
-        }//id equals type
+            else if(this.Url != null){
+              $('#' + args[i]).append(
+                $('<a class="list-group-item text-center list-link" ' +
+                'href="' + data[index].Url + '">' +
+                data[index].ArticleTitle +
+                '</a>')
+              ) //append
+            }
+          }//check Article type vs ele type
+        }//for loop
       });//each
     }//success
   });//ajax
