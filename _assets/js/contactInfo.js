1f9b2640fdf40a608d74df118624b1fbd62a6e72
Austin Cole <mail@acole.us>
2017-11-02 14:30:59 -0700

    Updated contactInfo, contactBlock, and contact form.

--- _assets/js/contactInfo.js
+++ _assets/js/contactInfo.js
@@ -18,7 +18,31 @@ function contactInfo() {
       // Append ID to  email button href
       $("#emailButton").attr('href', '/contact/#' + result.Id );
 
-      console.log(result);
+      //console.log(result); // Uncomment for debug
     });
+  });
+}
+
+function deptList() {
+  $(document).ready(function() {
+    var restURL = 'https://www.latah.id.us/api/getcontact';
+    var queryString = window.location.href.slice(window.location.href.indexOf("#"));
+    // console.log('QS: ' + queryString); // Uncomment to debug queryString results
+
+    $.getJSON(restURL, function(data) {
+      $('#department').html('<option id="0" disabled selected>Select Department</option>');
+      $.each(data, function(index, result) {
+        var dept = result.Department;
+        var id = '#' + result.Id;
+        //console.log(id + ': ' + dept); // Uncomment to debug
+        
+        if (id == queryString) {
+          $("#department").append('<option id="' + id + '" value="' + dept + '" selected>' + dept + '</option>');
+        } else {
+          $("#department").append('<option id="' + id + '" value="' + dept + '">' + dept + '</option>');
+        }
+      });
+    });
+
   });
 }
