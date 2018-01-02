53bceff56177c1b2cee4a7983ed94a573f79effd
Austin Cole <mail@acole.us>
2018-01-02 12:33:58 -0800

    Changed Dept Menu Dropdown to resize dynamically.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,3 +1,20 @@
+/* Set Dept Window to Scroll for Small Windows */
+// Set Scroll Based on Window Size
+$(window).on
+function setScroll(){
+  var dh = $("#deptNav").height();
+  var h = $(window).innerHeight() - $('#navFooter').height();
+  if (dh >  (h - 75)) {
+    $("#deptNav").addClass('pre-scrollable');
+  }
+}
+// Check Window Size on Page Load
+setScroll();
+// Dynamically Update Window Size
+$(window).resize(function() {
+  setScroll();
+});
+
 /* Keep Footer Year Current */
 // instantiate a moment object
 var NowMoment = moment().format("YYYY");
