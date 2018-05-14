733efceb7416c3bbf031d7c5c5b773d24537b9f1
Austin Cole <mail@acole.us>
2018-05-14 08:37:05 -0700

    Added browser compatibility alert.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -51,3 +51,13 @@ $(
     'color': '#FFFFFF'
   })
 );
+
+function browserCheck() {
+  var ua = navigator.userAgent;
+
+  if (ua.indexOf(.NET) > 0 ) {
+    alert("This browser is not supported. \n Please use Chrome or Firefox for a better user experience.")
+  } else if (ua.indexOf('Edge') > 0) {
+    alert("This browser does not fully support all of the features of this site. \n Please use Chrome or Firefox for a better user experience.")
+  }
+}
