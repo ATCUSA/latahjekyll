01be12f624a8e7bd7bacde6aa4fc9d4853f8d9f8
Austin Cole <mail@acole.us>
2018-05-14 09:26:13 -0700

    Fixed typo.

--- _assets/js/contactInfo.js
+++ _assets/js/contactInfo.js
@@ -187,7 +187,7 @@ function formSubmit() {
 function browserCheck() {
   var ua = navigator.userAgent;
 
-  if (ua.indexOf(.NET) > 0) {
+  if (ua.indexOf('.NET') > 0) {
     alert("This browser is not supported. \n Please use Chrome or Firefox for a better user experience.")
   } else if (ua.indexOf('Edge') > 0) {
     alert("This browser does not fully support all of the features of this site. \n Please use Chrome or Firefox for a better user experience.")
