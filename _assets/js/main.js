3c23caa5d39c4c34983a1ae841960cf46c1503eb
Todd Cherry <tscherry70@gmail.com>
2018-05-30 16:52:00 -0700

    added code to hide news or notice based on emply list group.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -26,8 +26,23 @@ var NowMoment = moment().format("YYYY");
 var eDisplayMoment = document.getElementById('currentYear');
 eDisplayMoment.innerHTML = NowMoment;
 
-// Makes the panels the same height
+
 $(function () {
+  //this code hides an empty news or notice panel and allows the 
+  //adjacent panel to take all of the remaining space
+  var n1 = $('#news .list-group').children().children().length;  
+  var n2 = $('#notices .list-group').children().children().length;
+    
+  if(n2 == 0) {
+      $('#notices').toggle();      
+      $('#news').removeClass().addClass('container col-md-12');      
+  }
+  if(n1 == 0){
+    $('#news').toggle();
+    $('#notices').removeClass().addClass('container col-md-12');
+  }
+
+  // Makes the panels the same height
   if ($('#newsNotices').length == 0) {
     $('.panel').matchHeight(
       {
@@ -41,7 +56,8 @@ $(function () {
       }
     );
   }
-});
+
+})
 
 // Changes panel heading from default to darker color
 $(
--- _assets/js/news.js
+++ _assets/js/news.js
@@ -42,23 +42,7 @@ function getNewsandNotices(args, dept){
                 '</a>')
               ) //append
             }            
-          }//check Article type vs ele type
-          else{            
-            
-            /* var id = $('#' + args[i]).attr('id');
-            $('list-group-item:contains("None at this time")').each(function(){
-              console.log($this);
-              if($(this).children().length == 0){
-
-              }                
-            });
-
-            
-            $('#' + args[i]).append(
-              $('<a class="list-group-item text-center"' + '>' + 'None at this time' + '</a>')
-            ) */
-            
-          }
+          }//check Article type vs ele type          
         }//for loop
       });//each
     }//success
