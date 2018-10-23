02d92fb9431ba648294bdd95393b2c008e5f2da9
Todd Cherry <tscherry70@gmail.com>
2018-10-23 15:47:11 -0700

    Added downloadJobApp function to the fileDownload module, updated BOCG job app link.

--- _assets/js/fileDownload.js
+++ _assets/js/fileDownload.js
@@ -2,3 +2,7 @@ function downloadFileEx(param1) {
       var url = 'https://api.latah.id.us/web/DownloadFileEx?filename=' + param1;
       window.open(url);
 }
+function downloadJobApp(filename){
+      var url = 'https://api.latah.id.us/web/downloadJobApp?fileName=' + filename;
+      window.open(url);
+}
